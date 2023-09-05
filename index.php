9f8014b024baa42343a63b0f82bc7fbe0fa5b699
TENSHKUMAR-KKT-2004 <tenshkumarkkt@gmail.com>
2023-09-05 19:35:51 +0530

    Landing Page Template & 3d Text Update

---
+++ index.php
@@ -0,0 +1,452 @@
+<?php ?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+<head>
+
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<title>Waves'23</title>
+<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+
+<link rel="stylesheet" href="public/css/bootstrap.min.css">
+
+<link rel="stylesheet" href="public/css/fontawesome-all.min.css">
+
+<link rel="stylesheet" href="public/css/swiper.min.css">
+
+<link rel="stylesheet" href="public/css/style.css">
+<script nonce="2af76b41-facd-47ce-9e71-e256854a4086">(function(w,d){!function(db,dc,dd,de){db[dd]=db[dd]||{};db[dd].executed=[];db.zaraz={deferred:[],listeners:[]};db.zaraz.q=[];db.zaraz._f=function(df){return async function(){var dg=Array.prototype.slice.call(arguments);db.zaraz.q.push({m:df,a:dg})}};for(const dh of["track","set","debug"])db.zaraz[dh]=db.zaraz._f(dh);db.zaraz.init=()=>{var di=dc.getElementsByTagName(de)[0],dj=dc.createElement(de),dk=dc.getElementsByTagName("title")[0];dk&&(db[dd].t=dc.getElementsByTagName("title")[0].text);db[dd].x=Math.random();db[dd].w=db.screen.width;db[dd].h=db.screen.height;db[dd].j=db.innerHeight;db[dd].e=db.innerWidth;db[dd].l=db.location.href;db[dd].r=dc.referrer;db[dd].k=db.screen.colorDepth;db[dd].n=dc.characterSet;db[dd].o=(new Date).getTimezoneOffset();if(db.dataLayer)for(const dp of Object.entries(Object.entries(dataLayer).reduce(((dq,dr)=>({...dq[1],...dr[1]})),{})))zaraz.set(dp[0],dp[1],{scope:"page"});db[dd].q=[];for(;db.zaraz.q.length;){const ds=db.zaraz.q.shift();db[dd].q.push(ds)}dj.defer=!0;for(const dt of[localStorage,sessionStorage])Object.keys(dt||{}).filter((dv=>dv.startsWith("_zaraz_"))).forEach((du=>{try{db[dd]["z_"+du.slice(7)]=JSON.parse(dt.getItem(du))}catch{db[dd]["z_"+du.slice(7)]=dt.getItem(du)}}));dj.referrerPolicy="origin";dj.src="/cdn-cgi/zaraz/s.js?z="+btoa(encodeURIComponent(JSON.stringify(db[dd])));di.parentNode.insertBefore(dj,di)};["complete","interactive"].includes(dc.readyState)?zaraz.init():db.addEventListener("DOMContentLoaded",zaraz.init)}(w,d,"zarazData","script");})(window,document);</script></head>
+<body>
+<header class="site-header">
+<div class="header-bar">
+<div class="container-fluid">
+<div class="row align-items-center">
+<div class="col-10 col-lg-4">
+<h1 class="site-branding flex">
+<a href="#">Waves'23</a>
+</h1>
+</div>
+<div class="col-2 col-lg-8">
+<nav class="site-navigation">
+<div class="hamburger-menu d-lg-none">
+<span></span>
+<span></span>
+<span></span>
+<span></span>
+</div>
+<ul>
+<li><a href="#">HOME</a></li>
+<li><a href="#">Waves'23 2018</a></li>
+<li><a href="#">ARTISTS</a></li>
+<li><a href="#">BLOG</a></li>
+<li><a href="#">CONTACT</a></li>
+<li><a href="#"><i class="fas fa-search"></i></a></li>
+</ul>
+</nav>
+</div>
+</div>
+</div>
+</div>
+</header>
+<div class="hero-content">
+<div class="container">
+<div class="row">
+<div class="col-12 offset-lg-2 col-lg-10">
+<div class="entry-header">
+<!-- <h2>Waves'23</h2> -->
+<div id="stage"></div>
+<canvas id="text" width="800" height="200"></canvas>
+<input id="input" type="text" value="Waves'23" style="display: none;"/>
+
+<!-- <div class="entry-meta-date">
+06.28.018
+</div> -->
+</div>
+<div class="countdown flex flex-wrap justify-content-between" data-date="2018/06/06">
+<div class="countdown-holder">
+<div class="dday"></div>
+<label>Days</label>
+</div>
+<div class="countdown-holder">
+<div class="dhour"></div>
+<label>Hours</label>
+</div>
+<div class="countdown-holder">
+<div class="dmin"></div>
+<label>Minutes</label>
+</div>
+<div class="countdown-holder">
+<div class="dsec"></div>
+<label>Seconds</label>
+</div>
+</div>
+</div>
+</div>
+<div class="row">
+<div class="col-12 ">
+<div class="entry-footer">
+<a href="#" class="btn">Buy Tickets</a>
+<a href="#" class="btn current">See Lineup</a>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="content-section">
+<div class="container">
+<div class="row">
+<div class="col-12">
+<div class="lineup-artists-headline">
+<div class="entry-title">
+<p>JUST THE BEST</p>
+<h2>The Lineup Artists-Headliners</h2>
+</div>
+<div class="lineup-artists">
+<div class="lineup-artists-wrap flex flex-wrap">
+<figure class="featured-image">
+<a href="#"> <img src="public/images/black-chick.jpg" alt> </a>
+</figure>
+<div class="lineup-artists-description">
+<div class="lineup-artists-description-container">
+<div class="entry-title">
+Jamila Williams
+</div>
+<div class="entry-content">
+<p>Quisque at erat eu libero consequat tempus. Quisque mole stie convallis tempus. Ut semper purus metus, a euismod sapien sodales ac. Duis viverra eleifend fermentum. </p>
+</div>
+<div class="box-link">
+<a href><img src="public/images/box.jpg" alt></a>
+</div>
+</div>
+</div>
+</div>
+<div class="lineup-artists-wrap flex flex-wrap">
+<div class="lineup-artists-description">
+<figure class="featured-image d-md-none">
+<a href="#"> <img src="public/images/mathew-kane.jpg" alt> </a>
+</figure>
+<div class="lineup-artists-description-container">
+<div class="entry-title">
+Sandra Superstar
+</div>
+<div class="entry-content">
+<p>Quisque at erat eu libero consequat tempus. Quisque mole stie convallis tempus. Ut semper purus metus, a euismod sapien sodales ac. Duis viverra eleifend fermentum. </p>
+</div>
+<div class="box-link">
+<a href="#"><img src="public/images/box.jpg" alt></a>
+</div>
+</div>
+</div>
+<figure class="featured-image d-none d-md-block">
+<a href="#"> <img src="public/images/mathew-kane.jpg" alt> </a>
+</figure>
+</div>
+<div class="lineup-artists-wrap flex flex-wrap">
+<figure class="featured-image">
+<a href="#"> <img src="public/images/eric-ward.jpg" alt> </a>
+</figure>
+<div class="lineup-artists-description">
+<div class="lineup-artists-description-container">
+<div class="entry-title">
+DJ Crazyhead
+</div>
+<div class="entry-content">
+<p>Quisque at erat eu libero consequat tempus. Quisque mole stie convallis tempus. Ut semper purus metus, a euismod sapien sodales ac. Duis viverra eleifend fermentum. </p>
+</div>
+<div class="box-link">
+<a href="#"> <img src="public/images/box.jpg" alt></a>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="row">
+<div class="col-12">
+<div class="the-complete-lineup">
+<div class="entry-title">
+<p>JUST THE BEST</p>
+<h2>The Complete Lineup</h2>
+</div>
+<div class="row the-complete-lineup-artists">
+<div class="col-6 col-md-4 col-lg-3 artist-single">
+<figure class="featured-image">
+<a href="#"> <img src="public/images/image-1.jpg" alt> </a>
+<a href="#" class="box-link"> <img src="public/images/box.jpg" alt> </a>
+</figure>
+<h2>Miska Smith</h2>
+</div>
+<div class="col-6 col-md-4 col-lg-3 artist-single">
+<figure class="featured-image">
+<a href="#"> <img src="public/images/image-2.jpg" alt> </a>
+<a href="#" class="box-link"> <img src="public/images/box.jpg" alt> </a>
+</figure>
+<h2>Hayley Down</h2>
+</div>
+<div class="col-6 col-md-4 col-lg-3 artist-single">
+<figure class="featured-image">
+<a href="#"> <img src="public/images/image-3.jpg" alt> </a>
+<a href="#" class="box-link"> <img src="public/images/box.jpg" alt> </a>
+</figure>
+<h2>The Band Song</h2>
+</div>
+<div class="col-6 col-md-4 col-lg-3 artist-single">
+<figure class="featured-image">
+<a href="#"> <img src="public/images/image-4.jpg" alt> </a>
+<a href="#" class="box-link"> <img src="public/images/box.jpg" alt> </a>
+</figure>
+<h2>Pink Machine</h2>
+</div>
+<div class="col-6 col-md-4 col-lg-3 artist-single">
+<figure class="featured-image">
+<a href="#"> <img src="public/images/image-5.jpg" alt> </a>
+<a href="#" class="box-link"> <img src="public/images/box.jpg" alt> </a>
+</figure>
+<h2>Brasil Band</h2>
+</div>
+<div class="col-6 col-md-4 col-lg-3 artist-single">
+<figure class="featured-image">
+<a href="#"> <img src="public/images/image-6.jpg" alt> </a>
+<a href="#" class="box-link"> <img src="public/images/box.jpg" alt> </a>
+</figure>
+<h2>Mickey</h2>
+</div>
+<div class="col-6 col-md-4 col-lg-3 artist-single">
+<figure class="featured-image">
+<a href="#"> <img src="public/images/image-7.jpg" alt> </a>
+<a href="#" class="box-link"> <img src="public/images/box.jpg" alt> </a>
+</figure>
+<h2>DJ Girl</h2>
+</div>
+<div class="col-6 col-md-4 col-lg-3 artist-single">
+<figure class="featured-image">
+<a href="#"> <img src="public/images/image-8.jpg" alt> </a>
+<a href="#" class="box-link"> <img src="public/images/box.jpg" alt> </a>
+</figure>
+<h2>Stan Smith</h2>
+</div>
+</div>
+<div class="row justify-content-center">
+<div class="see-complete-lineup">
+<div class="entry-footer">
+<a href="#" class="btn">See all lineup</a>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class="homepage-next-events">
+<div class="container">
+<div class="row">
+<div class="col-12">
+<div class="entry-title">
+<p>JUST THE BEST</p>
+<h2>Our Next Events</h2>
+</div>
+</div>
+</div>
+</div>
+<div class="next-event-slider-wrap">
+<div class="swiper-container next-event-slider">
+<div class="swiper-wrapper">
+<div class="swiper-slide">
+<div class="next-event-content">
+<figure class="featured-image">
+<img src="public/images/next-event-1.jpg" alt>
+<a href="#" class="entry-content flex flex-column justify-content-center align-items-center">
+<h3>Welcoming Party 2018</h3>
+<p>Green Palace, 22 Street, 23-28, Los Angeles California</p>
+</a>
+</figure>
+</div>
+</div>
+<div class="swiper-slide">
+<div class="next-event-content">
+<figure class="featured-image">
+<img src="public/images/next-event-2.jpg" alt>
+<a href="#" class="entry-content flex flex-column justify-content-center align-items-center">
+<h3>Welcoming Party 2018</h3>
+<p>Green Palace, 22 Street, 23-28, Los Angeles California</p>
+</a>
+</figure>
+</div>
+</div>
+<div class="swiper-slide">
+<div class="next-event-content">
+<figure class="featured-image">
+<img src="public/images/next-event-3.jpg" alt>
+<a href="#" class="entry-content flex flex-column justify-content-center align-items-center">
+<h3>Welcoming Party 2018</h3>
+<p>Green Palace, 22 Street, 23-28, Los Angeles California</p>
+</a>
+</figure>
+</div>
+</div>
+<div class="swiper-slide">
+<div class="next-event-content">
+<figure class="featured-image">
+<img src="public/images/next-event-1.jpg" alt>
+<a href="#" class="entry-content flex flex-column justify-content-center align-items-center">
+<h3>Welcoming Party 2018</h3>
+<p>Green Palace, 22 Street, 23-28, Los Angeles California</p>
+</a>
+</figure>
+</div>
+</div>
+<div class="swiper-slide">
+<div class="next-event-content">
+<figure class="featured-image">
+<img src="public/images/next-event-2.jpg" alt>
+<a href="#" class="entry-content flex flex-column justify-content-center align-items-center">
+<h3>Welcoming Party 2018</h3>
+<p>Green Palace, 22 Street, 23-28, Los Angeles California</p>
+</a>
+</figure>
+</div>
+</div>
+<div class="swiper-slide">
+<div class="next-event-content">
+<figure class="featured-image">
+<img src="public/images/next-event-3.jpg" alt>
+<a href="#" class="entry-content flex flex-column justify-content-center align-items-center">
+<h3>Welcoming Party 2018</h3>
+<p>Green Palace, 22 Street, 23-28, Los Angeles California</p>
+</a>
+</figure>
+</div>
+</div>
+</div>
+</div>
+<div class="swiper-button-next">
+<img src="public/images/button.png" alt>
+</div>
+</div>
+</div>
+<div class="home-page-last-news">
+<div class="container">
+<div class="header">
+<div class="entry-title">
+<p>JUST THE BEST</p>
+<h2>Our Last News</h2>
+</div>
+</div>
+<div class="home-page-last-news-wrap">
+<div class="row">
+<div class="col-12 col-md-6">
+<figure class="featured-image">
+<a href="#"> <img src="public/images/news-image-1.jpg" alt="fesival+celebration"> </a>
+</figure>
+<div class="box-link-date">
+<a href="#">03.12.18</a>
+</div>
+<div class="content-wrapper">
+<div class="entry-content">
+<div class="entry-header">
+<h2><a href="#">10 Festival Tips</a></h2>
+</div>
+<div class="entry-meta">
+<span class="author-name"><a href="#"> By James Williams</a></span>
+<span class="space">|</span>
+<span class="comments-count"><a href="#">3 comments</a></span>
+</div>
+<div class="entry-description">
+<p>Quisque at erat eu libero consequat tempus.
+Quisque mole stie convallis tempus.
+Ut semper purus metus, a euismod sapien sodales ac.
+Duis viverra eleifend fermentum.</p>
+</div>
+</div>
+</div>
+</div>
+<div class="col-12 col-md-6">
+<figure class="featured-image">
+<a href="#"> <img src="public/images/news-image-1.jpg" alt="fesival+celebration"> </a>
+</figure>
+<div class="box-link-date">
+<a href>03.12.18</a>
+</div>
+<div class="content-wrapper">
+<div class="entry-content">
+<div class="entry-header">
+<h2><a href="#">New event calendar for this year</a></h2>
+</div>
+<div class="entry-meta">
+<span class="author-name"><a href="#">By James Williams</a></span>
+<span class="space">|</span>
+<span class="comments-count"><a href="#">3 comments</a></span>
+</div>
+<div class="entry-description">
+<p>Quisque at erat eu libero consequat tempus.
+Quisque mole stie convallis tempus.
+Ut semper purus metus, a euismod sapien sodales ac.
+Duis viverra eleifend fermentum.</p>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<footer class="site-footer">
+<div class="footer-cover-title flex justify-content-center align-items-center">
+<h2>Waves'23</h2>
+</div>
+<div class="footer-content-wrapper">
+<div class="container">
+<div class="row">
+<div class="col-12">
+<div class="entry-title">
+<a href="#">Waves'23</a>
+</div>
+<div class="entry-mail">
+<a href="#"><span class="__cf_email__" data-cfemail="8bd8cad2c3cec7c7c4cbd8dec5cdced8dfa5c8c4c6">[email&#160;protected]</span></a>
+</div>
+<div class="copyright-info">
+
+Copyright &copy;<script data-cfasync="false" src="public/cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script><script>document.write(new Date().getFullYear());</script> Design & Developed by <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://nscet.org/ispin/" target="_blank">iSPIN</a>
+
+</div>
+<div class="footer-social">
+<ul class="flex justify-content-center align-items-center">
+<li><a href="#"><i class="fab fa-pinterest"></i></a></li>
+<li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+<li><a href="#"><i class="fab fa-twitter"></i></a></li>
+<li><a href="#"><i class="fab fa-dribbble"></i></a></li>
+<li><a href="#"><i class="fab fa-behance"></i></a></li>
+<li><a href="#"><i class="fab fa-linkedin-in"></i></a></li>
+</ul>
+</div>
+</div>
+</div>
+</div>
+</div>
+</footer>
+<script type="text/javascript" src="public/js/jquery.js"></script>
+<script type="text/javascript" src="public/js/masonry.pkgd.min.js"></script>
+<script type="text/javascript" src="public/js/jquery.collapsible.min.js"></script>
+<script type="text/javascript" src="public/js/swiper.min.js"></script>
+<script type="text/javascript" src="public/js/jquery.countdown.min.js"></script>
+<script type="text/javascript" src="public/js/circle-progress.min.js"></script>
+<script type="text/javascript" src="public/js/jquery.countTo.min.js"></script>
+<script type="text/javascript" src="public/js/custom.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/three.js/r71/three.min.js"></script>
+<script type="text/javascript" src="public/js/3d-text.js"></script>
+
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-23581568-13');
+</script>
+<script defer src="https://static.cloudflareinsights.com/beacon.min.js/v8b253dfea2ab4077af8c6f58422dfbfd1689876627854" integrity="sha512-bjgnUKX4azu3dLTVtie9u6TKqgx29RBwfj3QXYt5EKfWM/9hPSAI/4qcV5NACjwAo8UtTeWefx6Zq5PHcMm7Tg==" data-cf-beacon='{"rayId":"801ca2883dc3859f","token":"cd0b4b3a733644fc843ef0b185f98241","version":"2023.8.0","si":100}' crossorigin="anonymous"></script>
+</body>
+</html>
