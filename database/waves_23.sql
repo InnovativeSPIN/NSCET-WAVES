f186f5268d807f5b5bd82b9f1c3244e5fddcdfa2
MUKILAN S <mukilan069@gmail.com>
2023-09-06 12:54:24 +0530

    DB Tabel Intialize

--- database/waves_23.sql
+++ database/waves_23.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 06, 2023 at 08:06 AM
+-- Generation Time: Sep 06, 2023 at 09:23 AM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.0.25
 
@@ -24,13 +24,76 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `admindb`
+--
+
+CREATE TABLE `admindb` (
+  `name` varchar(255) NOT NULL,
+  `dept` varchar(255) NOT NULL,
+  `role` varchar(255) NOT NULL,
+  `reg_no` int(11) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `event_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `eventdb`
+--
+
+CREATE TABLE `eventdb` (
+  `event_name` varchar(255) NOT NULL,
+  `event_id` int(11) NOT NULL,
+  `event_date` date NOT NULL,
+  `event_time` varchar(255) NOT NULL,
+  `event_venue` varchar(255) NOT NULL,
+  `max_participants` int(11) NOT NULL,
+  `is_group` int(11) NOT NULL,
+  `group_counts` int(11) NOT NULL,
+  `group_participants` int(11) NOT NULL,
+  `allowance` int(11) NOT NULL,
+  `gender` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `housedb`
 --
 
 CREATE TABLE `housedb` (
   `name` varchar(255) DEFAULT NULL,
   `id` int(11) NOT NULL,
-  `score` int(11) DEFAULT NULL
+  `score` int(11) DEFAULT NULL,
+  `gender` varchar(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `housedb`
+--
+
+INSERT INTO `housedb` (`name`, `id`, `score`, `gender`) VALUES
+('TIGER THRASHERS', 921001, 0, 'BOYS'),
+('DRAGON WARRIORS', 921002, 0, 'BOYS'),
+('PHOENIX BLASTERS', 921003, 0, 'BOYS'),
+('DINO THUNDERS', 921004, 0, 'BOYS'),
+('ROSY RIDERS', 921005, 0, 'GIRLS'),
+('VIOLET VIPERS', 921006, 0, 'GIRLS'),
+('GALACTIC STARS', 921007, 0, 'GIRLS'),
+('BLUE BLASTERS', 921008, 0, 'GIRLS');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `registerationdb`
+--
+
+CREATE TABLE `registerationdb` (
+  `reg_no` int(11) NOT NULL,
+  `event_id` int(11) NOT NULL,
+  `house_id` int(11) NOT NULL,
+  `grouped` int(11) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -68,12 +131,6 @@ ALTER TABLE `studentdb`
 --
 -- AUTO_INCREMENT for dumped tables
 --
-
---
--- AUTO_INCREMENT for table `housedb`
---
-ALTER TABLE `housedb`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `studentdb`
