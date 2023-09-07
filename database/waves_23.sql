1a42b5c9ffc29257bb0d496f3e7d1193bd0c601f
MADAVAN K <kmadavan018@gmail.com>
2023-09-07 23:24:26 +0530

    Event list added

--- database/waves_23.sql
+++ database/waves_23.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 07, 2023 at 04:52 PM
--- Server version: 10.4.27-MariaDB
--- PHP Version: 8.0.25
+-- Generation Time: Sep 07, 2023 at 07:53 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -68,7 +68,20 @@ INSERT INTO `eventdb` (`event_name`, `event_id`, `event_date`, `event_time`, `ev
 ('CARBON ART', 1, '26/09/23', '09:45 AM – 10-45 AM', 'Chemistry Lab', 2, 0, 0, 0, 2, 'COMMON', 'public/images/event/carbonart.jpg', 'Off Stage', 'It is solo event. This event is for both boys and girls teams. Participants count : 2 per team. Time limit : 1 hour. The topic will be given on the spot.'),
 ('CARVO', 2, '27/09/23', '02:00 PM to 03:00 PM', 'Ohm’s Lab', 6, 1, 2, 3, 6, 'COMMON', 'public/images/event/carvo.jpg', 'Off Stage', 'It is team event. This event is for both boys and girls teams. Participants count : 2 groups/ team with 3 members in each group. Time limit : 1 hour. The participants should bring all necessary materials.'),
 ('ARTY CRAFTY', 3, '26/09/23', '12:00 PM to 01:00 PM', 'Ohm’s Lab', 4, 1, 2, 2, 4, 'COMMON', 'public/images/event/ARTYCRAFTY.png', 'Off Stage', 'It is team event This event is for both boys and girls teams. Participants count : 2 groups/ team with 2 members in each group. Time limit : 1 hour. The participants should bring necessary materials. Only waste materials should be used'),
-('PICASSO ON GLASS', 4, '26/09/23', '11:00 AM to 11:45 AM', 'Chemistry Lab', 4, 1, 2, 2, 4, 'COMMON', 'public/images/event/glasspicaso.jpg', 'Off Stage', 'It is team event. This event is for both boys and girls teams. Participants count : 2 groups/ team with 2 members in each group. Time limit : 1 hour. The participants should bring all necessary materials.');
+('PICASSO ON GLASS', 4, '26/09/23', '11:00 AM to 11:45 AM', 'Chemistry Lab', 4, 1, 2, 2, 4, 'COMMON', 'public/images/event/glasspicaso.jpg', 'Off Stage', 'It is team event. This event is for both boys and girls teams. Participants count : 2 groups/ team with 2 members in each group. Time limit : 1 hour. The participants should bring all necessary materials.'),
+('BLOSSOM & CRAFTS', 5, '26/09/23', '02:00 PM to 03:00 PM', 'Chemistry Lab', 4, 1, 2, 2, 4, 'COMMON', 'public/images/event/several-colorful-gifts-with-bow.jpg', 'Off Stage', 'It is team event. This event is for both boys and girls teams. Participants count : 2 groups/ team with 2 members in each group. Time limit : 1 hour. The participants should bring all necessary materials. The art may be floral or any gift and it depends o'),
+('SNAPOGRAPHY', 6, '27/09/23', '02:45 PM to 03:45 PM', 'Language Lab', 4, 1, 2, 2, 4, 'COMMON', 'public/images/event/photography-camera-learning-feature.JPG', 'Off Stage', 'It is team event. This event is for boys teams only. Participants count : 2 groups/ team with 2 members in each group. Time limit : 1 hour. The participants should bring professional camera. Use of mobile phone is prohibited. Theme will be given on the sp'),
+('CUISINE MASTER', 7, '26/09/23', '03:15 PM to 04:15 PM', 'Physics Lab', 6, 1, 2, 3, 6, 'COMMON', 'public/images/event/cuisine_master.jpg', 'Off Stage', 'It is team event. This event is for Girls teams only. Participants count : 2 groups/ team with 3 members in each group. Time limit : 1 hour. The participants should bring all necessary materials. Results will be based on taste and creativity. Participants'),
+('TALK ON THE SPOT', 8, '26/09/23', '01:00 PM to 02:00 PM', 'Language Lab', 4, 1, 2, 2, 4, 'COMMON', 'public/images/event/talk.jpg', 'Off Stage', 'It is solo event. This event is for both boys and girls teams. Participants count : 2 per team. Time limit : 2 minutes for preparation, 3 minutes for performance. The topic will be given on the spot. Judgment will be based on relevance to the title, fluen'),
+('TECH QUEST CHALLENGE', 11, '26/09/23', '03:30 PM to 04:30 PM', 'Sir. C. V. Raman Auditorium', 4, 1, 1, 4, 4, 'COMMON', 'public/images/event/treasure-hunt.avif', 'Off Stage', 'It is team event. This event is for both boys and girls teams. Participants count : Maximum 4 members. Time limit : 1 hour. Detailed rules will be announced by organizers.'),
+('HANDY HENNA', 12, '27/09/23', '09:45 AM – 10-45 AM', 'Chemistry Lab', 4, 1, 2, 2, 4, 'GIRLS', 'public/images/event/henna.avif', 'Off Stage', 'It is team event. This event is for Girls teams only. Participants count : 2 groups/ team with 2 members in each group. Time limit : 1 hour. The participants should bring all necessary materials.'),
+('HAIRY DORA', 13, '27/09/23', '11:00 AM to 12:00 noon', 'Physics Lab', 4, 1, 2, 2, 4, 'GIRLS', 'public/images/event/hairy.jpg', 'Off Stage', 'It is team event. This event is for Girls teams only. Participants count : 2 groups/ team with 2 members in each group. Time limit : 1 hour. The participants should bring all necessary materials.'),
+('WARRIOR\'S SHOWCASE', 15, '27/09/23', '09:45 AM – 10-45 AM', 'PlayGround', 2, 1, 1, 2, 2, 'BOYS', 'public/images/event/martial-arts.webp', 'Off Stage', 'It is team event. This event is for boys teams only. Participants count : 2 members in each Team. Time limit : 5 minutes. The participants should bring all necessary materials. The performance may be Yoga, Karate, silambam, karali, kattas, takewan-do mov'),
+('PICASSO ON FACE', 16, '27/09/23', '11:00 AM to 12:00 noon', 'Ohm’s Lab', 4, 1, 2, 2, 4, 'BOYS', 'public/images/event/face-painting.avif', 'Off Stage', 'It is team event. This event is for boys teams only. Participants count : 2 groups/ team with 2 members in each group. Time limit : 1 hour. The participants should bring all necessary materials. Theme will be given on the spot.'),
+('ANY BODY CAN DANCE', 18, '29/09/23', '12:45 PM to 02:45 PM', 'Kamarajar Open Auditorium', 10, 1, 1, 10, 10, 'COMMON', 'public/images/event/dance.avif', 'On Stage', 'It is team event. This event is for both boys and girls teams. Participants count : Maximum 10. Time limit : 08minutes : 5 minutes own choice, 3 minutes ‘foot loose’. Song selection and dance should be purely westernized. Any deviation in the rules will l'),
+('CHOREO BOOM', 19, '30/09/23', '10:00 AM to 12:00 noon', 'Kamarajar Open Auditorium', 10, 1, 1, 10, 10, 'COMMON', 'public/images/event/dance2.jpg', 'On Stage', 'It is team event. This event is for both boys and girls teams. Participants count : Maximum 10. Time limit : 10 minutes : 7 minutes own choice, 3 minutes ‘foot loose’. Song selection and dance should be purely folk. Any deviation in the rules will lead to'),
+('MIME ODYSSEY', 20, '29/09/23', '02:45 PM to 03:45 PM', 'Kamarajar Open Auditorium', 10, 1, 1, 10, 10, 'COMMON', 'public/images/event/mime.avif', 'On Stage', 'It is team event. This event is for both boys and girls teams. Participants count : Maximum 10. Time limit : 05 minutes. The theme should be informed to the organizers and event coordinator two days before the conduct of event. The theme of the event shou'),
+('MUSICAL RHAPSODY', 21, '29/09/23', '03:45 PM to 04:45 PM', 'Kamarajar Open Auditorium', 1, 0, 1, 1, 1, 'COMMON', 'public/images/event/sing.jpg', 'On Stage', 'It is solo event. This event is for both boys and girls teams. Time limit : 5 minutes. Participants count : 1 per team. The karoke of the performing song should be submitted to the event coordinators and verified on the day before event.');
 
 -- --------------------------------------------------------
 
