b5f8057ddc2aa0e70bf9d4f9e8961651b9f33d8a
TENSHKUMAR-KKT-2004 <tenshkumarkkt@gmail.com>
2023-09-14 11:06:27 +0530

    Team Leads Details adding

--- pages/studentDashboard.php
+++ pages/studentDashboard.php
@@ -9,8 +9,8 @@ if (!isset($_SESSION['name']) && !isset($_SESSION['reg_no'])) {
 $role = $_SESSION['role'];
 
 if ($role != 'student') {
-    header('Location: /');
-    exit();
+	header('Location: /');
+	exit();
 }
 
 include('../routes/connect.php');
@@ -24,7 +24,8 @@ include('../routes/connect.php');
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Student | Dashboard</title>
 
-	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+		integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	<link rel="stylesheet" href="../public/css/studentDashboardStyles.css">
 	<link rel="stylesheet" href="../public/css/houseDashboardStyles.css">
 	<link rel="stylesheet" href="../public/css/style.css">
@@ -97,122 +98,156 @@ include('../routes/connect.php');
 							Your Year:
 							<?php echo $_SESSION['year'] ?>
 						</h5>
-						<!-- <h5 class="card-text">
-                        Vice Captain:
-                    </h5> -->
 					</div>
+					<?php
+					$house_name = $_SESSION['house'];
+					$queryHouseLeadsName = "SELECT name FROM `admindb` WHERE house_name = '$house_name'";
+					$getHouseLeadsResult = mysqli_query($conn, $queryHouseLeadsName);
 
+					$houseLeads = array();
+					while ($houseLead = mysqli_fetch_array($getHouseLeadsResult)) {
+						array_push($houseLeads, $houseLead['name']);
+					}
+					?>
+					<div class="card-body">
+						<div class="text-section">
+							<h1 class='card-title'> House Leads Details
+							</h1>
+							<h5 class="card-text">
+								Team Incharges:
+								<?php echo $houseLeads[0].', '.$houseLeads[1] ?>
+							</h5>
+							<h5 class="card-text">
+								Team Captain:
+								<?php echo $houseLeads[2] ?>
+							</h5>
+							<h5 class="card-text">
+								Team Vice Captain:
+								<?php echo $houseLeads[3] ?>
+							</h5>
+						</div>
+					</div>
 				</div>
 			</div>
-		</div>
-		<div class="row">
-			<h2 style="color: black;">Your Events</h2>
-
-			<?php
-			$student_reg_no = $_SESSION['reg_no'];
-			$registeredEvents = mysqli_query($conn, "SELECT * FROM `registerationdb` WHERE reg_no = $student_reg_no");
-			while ($registeredEvent = mysqli_fetch_array($registeredEvents)) {
-			?>
+			<div class="row">
+				<h2 style="color: black;">Your Events</h2>
 
 				<?php
-				$eventName = $registeredEvent['event_name'];
-				$eventDetails = mysqli_query($conn, "SELECT * FROM `eventdb` WHERE event_name = '$eventName'");
-				$eventDetail =  mysqli_fetch_array($eventDetails);
+				$student_reg_no = $_SESSION['reg_no'];
+				$registeredEvents = mysqli_query($conn, "SELECT * FROM `registerationdb` WHERE reg_no = $student_reg_no");
+				while ($registeredEvent = mysqli_fetch_array($registeredEvents)) {
+					?>
 
-				$houseName = $registeredEvent['student_house'];
-				$houseDetails = mysqli_query($conn, "SELECT * FROM `housedb` WHERE name = '$houseName'");
-				$houseDetail =  mysqli_fetch_array($houseDetails);
-				?>
-				<!-- Modal -->
-				<div class="modal fade <?php echo $eventDetail['event_id'] ?>" tabindex="-1" role="dialog" aria-labelledby="<?php echo $eventDetail['event_id'] ?>" aria-hidden="true">
-					<div class="modal-dialog" role="document">
-						<div class="modal-content" style="padding: 23px;">
-							<div class="modal-header">
-								<h4 class="modal-title" id="exampleModalLabel" style="color: #e22361;">
-									<?php echo $eventDetail['event_name'] ?>
-								</h4>
-								<button type="button" class="close" data-dismiss="modal" aria-label="Close">
-									<span aria-hidden="true">&times;</span>
-								</button>
-							</div>
-							<div class="modal-body">
-								<div>
-									<div style="margin: 12px; text-align: center;">
-										<img src="<?php echo '../' . $eventDetail['image'] ?>" alt="" srcset="" width="270px" height="170px" style="margin: 22px;">
+					<?php
+					$eventName = $registeredEvent['event_name'];
+					$eventDetails = mysqli_query($conn, "SELECT * FROM `eventdb` WHERE event_name = '$eventName'");
+					$eventDetail = mysqli_fetch_array($eventDetails);
 
-										<h4>
-											<?php echo $eventDetail['event_date'] ?>
-										</h4>
-										<h4>
-											<?php echo $eventDetail['event_time'] ?>
-										</h4>
-										<h4>
-											<?php echo $eventDetail['event_venue'] ?>
-										</h4>
+					$houseName = $registeredEvent['student_house'];
+					$houseDetails = mysqli_query($conn, "SELECT * FROM `housedb` WHERE name = '$houseName'");
+					$houseDetail = mysqli_fetch_array($houseDetails);
+					?>
+					<!-- Modal -->
+					<div class="modal fade <?php echo $eventDetail['event_id'] ?>" tabindex="-1" role="dialog"
+						aria-labelledby="<?php echo $eventDetail['event_id'] ?>" aria-hidden="true">
+						<div class="modal-dialog" role="document">
+							<div class="modal-content" style="padding: 23px;">
+								<div class="modal-header">
+									<h4 class="modal-title" id="exampleModalLabel" style="color: #e22361;">
+										<?php echo $eventDetail['event_name'] ?>
+									</h4>
+									<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+										<span aria-hidden="true">&times;</span>
+									</button>
+								</div>
+								<div class="modal-body">
+									<div>
+										<div style="margin: 12px; text-align: center;">
+											<img src="<?php echo '../' . $eventDetail['image'] ?>" alt="" srcset=""
+												width="270px" height="170px" style="margin: 22px;">
 
-										<p>Event Cordinators
-											<br />
-											<?php $coordinators = explode("|", $eventDetail['event_cordinators']);
-											foreach ($coordinators as $letter => $index) {
-												echo '<span>' . $coordinators[$letter] . '<br /></span>';
+											<h4>
+												<?php echo $eventDetail['event_date'] ?>
+											</h4>
+											<h4>
+												<?php echo $eventDetail['event_time'] ?>
+											</h4>
+											<h4>
+												<?php echo $eventDetail['event_venue'] ?>
+											</h4>
+
+											<p>Event Cordinators
+												<br />
+												<?php $coordinators = explode("|", $eventDetail['event_cordinators']);
+												foreach ($coordinators as $letter => $index) {
+													echo '<span>' . $coordinators[$letter] . '<br /></span>';
+												}
+												?>
+											</p>
+										</div>
+										<h4 style="color: #e22361;">Rules</h4>
+										<p>
+
+											<?php $rules = explode(".", $eventDetail['event_rules']);
+											array_pop($rules);
+											foreach ($rules as $letter => $index) {
+												echo ($letter + 1) . '. ' . $rules[$letter] . '<br />';
 											}
+
+
 											?>
 										</p>
 									</div>
-									<h4 style="color: #e22361;">Rules</h4>
-									<p>
 
-										<?php $rules = explode(".", $eventDetail['event_rules']);
-										array_pop($rules);
-										foreach ($rules as $letter => $index) {
-											echo ($letter + 1) . '. ' . $rules[$letter] . '<br />';
+								</div>
+								<div class="modal-footer">
+									<button type="button" class="btn btn-secondary" data-dismiss="modal"></button>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="col-md-12">
+						<div data-toggle="modal" data-target=".<?php echo $eventDetail['event_id'] ?>"
+							class="single-schedule-area d-flex flex-wrap justify-content-between align-items-center wow fadeInUp"
+							data-wow-delay="300ms">
+
+							<div class="single-schedule-tumb-info d-flex align-items-center">
+
+								<div class="single-schedule-tumb">
+									<img height="100px" width="100px" src="<?php echo '../' . $eventDetail['image'] ?>">
+								</div>
+
+								<div class="single-schedule-info">
+									<h6>
+										<?php echo $eventDetail['event_name'] ?>
+									</h6>
+									<p>Cordinators,
+										<br />
+										<?php $coordinators = explode("|", $eventDetail['event_cordinators']);
+										foreach ($coordinators as $letter => $index) {
+											echo '<span>' . $coordinators[$letter] . '<br /></span>';
 										}
-
-
 										?>
 									</p>
 								</div>
-
-							</div>
-							<div class="modal-footer">
-								<button type="button" class="btn btn-secondary" data-dismiss="modal"></button>
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-12">
-					<div data-toggle="modal" data-target=".<?php echo $eventDetail['event_id'] ?>" class="single-schedule-area d-flex flex-wrap justify-content-between align-items-center wow fadeInUp" data-wow-delay="300ms">
-
-						<div class="single-schedule-tumb-info d-flex align-items-center">
-
-							<div class="single-schedule-tumb">
-								<img height="100px" width="100px" src="<?php echo '../' . $eventDetail['image'] ?>">
 							</div>
 
-							<div class="single-schedule-info">
-								<h6><?php echo $eventDetail['event_name'] ?></h6>
-								<p>Cordinators,
-									<br />
-									<?php $coordinators = explode("|", $eventDetail['event_cordinators']);
-									foreach ($coordinators as $letter => $index) {
-										echo '<span>' . $coordinators[$letter] . '<br /></span>';
-									}
-									?>
+							<div class="schedule-time-place">
+								<p><i class="zmdi zmdi-time"></i>
+									<?php echo $eventDetail['event_date'] ?> |
+									<?php echo $eventDetail['event_time'] ?>
+								</p>
+								<p><i class="zmdi zmdi-map"></i>
+									<?php echo $eventDetail['event_venue'] ?>
 								</p>
 							</div>
-						</div>
 
-						<div class="schedule-time-place">
-							<p><i class="zmdi zmdi-time"></i><?php echo $eventDetail['event_date'] ?> | <?php echo $eventDetail['event_time'] ?></p>
-							<p><i class="zmdi zmdi-map"></i> <?php echo $eventDetail['event_venue'] ?></p>
+							<a class="btn confer-btn">Detail <i class="zmdi zmdi-long-arrow-right"></i></a>
 						</div>
-
-						<a class="btn confer-btn">Detail <i class="zmdi zmdi-long-arrow-right"></i></a>
 					</div>
-				</div>
 
 
-				<!-- <div class="col">
+					<!-- <div class="col">
 					<div class="registered-details-card one">
 						<div class="top" style="background: url(<?php echo '../' . $eventDetail['image'] ?>) no-repeat;">
 							<div class="wrapper">
@@ -250,76 +285,80 @@ include('../routes/connect.php');
 					</div>
 				</div> -->
 
-			<?php
-			}
-			?>
+					<?php
+				}
+				?>
+			</div>
 		</div>
-	</div>
 
 
 
 
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js"></script>
-	<script type="text/javascript" src="../public/js/jquery.js"></script>
-	<script type="text/javascript" src="../public/js/jquery.js"></script>
-	<script type="text/javascript" src="../public/js/masonry.pkgd.min.js"></script>
-	<script type="text/javascript" src="../public/js/jquery.collapsible.min.js"></script>
-	<script type="text/javascript" src="../public/js/swiper.min.js"></script>
-	<script type="text/javascript" src="../public/js/jquery.countdown.min.js"></script>
-	<script type="text/javascript" src="../public/js/circle-progress.min.js"></script>
-	<script type="text/javascript" src="../public/js/jquery.countTo.min.js"></script>
-	<script type="text/javascript" src="../public/js/custom.js"></script>
-	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
-	</script>
-	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
-	</script>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
-	</script>
-	<script src="https://cdn.jsdelivr.net/npm/tsparticles-confetti@2.12.0/tsparticles.confetti.bundle.min.js"></script>
-	<script>
-		const duration = 10 * 1000,
-			animationEnd = Date.now() + duration,
-			defaults = {
-				startVelocity: 30,
-				spread: 720,
-				ticks: 60,
-				zIndex: 0
-			};
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js"></script>
+		<script type="text/javascript" src="../public/js/jquery.js"></script>
+		<script type="text/javascript" src="../public/js/jquery.js"></script>
+		<script type="text/javascript" src="../public/js/masonry.pkgd.min.js"></script>
+		<script type="text/javascript" src="../public/js/jquery.collapsible.min.js"></script>
+		<script type="text/javascript" src="../public/js/swiper.min.js"></script>
+		<script type="text/javascript" src="../public/js/jquery.countdown.min.js"></script>
+		<script type="text/javascript" src="../public/js/circle-progress.min.js"></script>
+		<script type="text/javascript" src="../public/js/jquery.countTo.min.js"></script>
+		<script type="text/javascript" src="../public/js/custom.js"></script>
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+			integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+			</script>
+		<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+			integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+			</script>
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+			integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+			</script>
+		<script
+			src="https://cdn.jsdelivr.net/npm/tsparticles-confetti@2.12.0/tsparticles.confetti.bundle.min.js"></script>
+		<script>
+			const duration = 10 * 1000,
+				animationEnd = Date.now() + duration,
+				defaults = {
+					startVelocity: 30,
+					spread: 720,
+					ticks: 60,
+					zIndex: 0
+				};
 
-		function randomInRange(min, max) {
-			return Math.random() * (max - min) + min;
-		}
-
-		const interval = setInterval(function() {
-			const timeLeft = animationEnd - Date.now();
-
-			if (timeLeft <= 0) {
-				return clearInterval(interval);
+			function randomInRange(min, max) {
+				return Math.random() * (max - min) + min;
 			}
 
-			const particleCount = 20 * (timeLeft / duration);
+			const interval = setInterval(function () {
+				const timeLeft = animationEnd - Date.now();
 
-			// since particles fall down, start a bit higher than random
-			confetti(
-				Object.assign({}, defaults, {
-					particleCount,
-					origin: {
-						x: randomInRange(0.1, 0.3),
-						y: Math.random() - 0.2
-					},
-				})
-			);
-			confetti(
-				Object.assign({}, defaults, {
-					particleCount,
-					origin: {
-						x: randomInRange(0.7, 0.9),
-						y: Math.random() - 0.2
-					},
-				})
-			);
-		}, 250);
-	</script>
+				if (timeLeft <= 0) {
+					return clearInterval(interval);
+				}
+
+				const particleCount = 20 * (timeLeft / duration);
+
+				// since particles fall down, start a bit higher than random
+				confetti(
+					Object.assign({}, defaults, {
+						particleCount,
+						origin: {
+							x: randomInRange(0.1, 0.3),
+							y: Math.random() - 0.2
+						},
+					})
+				);
+				confetti(
+					Object.assign({}, defaults, {
+						particleCount,
+						origin: {
+							x: randomInRange(0.7, 0.9),
+							y: Math.random() - 0.2
+						},
+					})
+				);
+			}, 250);
+		</script>
 </body>
 
 </html>
