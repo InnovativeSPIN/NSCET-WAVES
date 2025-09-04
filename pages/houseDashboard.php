b707f2800dec9ba029ee1b1b6f625a30891cdc93
B.Naveen Bharathi <144258519+bnaveenbharathi@users.noreply.github.com>
2025-09-04 14:10:25 +0530

    update events

--- pages/houseDashboard.php
+++ pages/houseDashboard.php
@@ -183,6 +183,7 @@ $path = '../public/images/house/';
 $img  = str_replace(" ", "_", $_SESSION['house_name']);
 
 if ($_SESSION['house_name'] == "EMERALD_EAGLES") {
+    echo $_SESSION['house_name'];
     echo '<img src="../public/images/house/EMERALD_EAGLES.png" class="card-img-top" alt="...">';
 } else {
     echo '<img src="' . $path . $img . '.png" class="card-img-top" alt="...">';
