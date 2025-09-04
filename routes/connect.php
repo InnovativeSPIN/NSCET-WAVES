63e8649329ac9ba9086af25ef76f448f83a5a567
Srihari Prasath <hari54stark@gmail.com>
2025-09-04 21:26:59 +0530

    db

--- routes/connect.php
+++ routes/connect.php
@@ -1,13 +1,13 @@
 <?php
 
-$conn = mysqli_connect('localhost', 'root', '', 'nscet_waves_24');        // make sql db connection
+// $conn = mysqli_connect('localhost', 'root', '', 'nscet_waves_25');        // make sql db connection
+// if (!$conn) {
+//     echo 'Connection Error  ' . mysqli_connect_error();
+// }
+
+$conn = mysqli_connect('localhost', 'nscet_waves_25', 'nscet_waves_25', 'nscet_waves_25');        
 if (!$conn) {
     echo 'Connection Error  ' . mysqli_connect_error();
 }
-
-// $conn = mysqli_connect('localhost', 'nscet_waves_25', 'nscet_waves_25', 'nscet_waves_25');        
-// if (!$conn) {
-    // echo 'Connection Error  ' . mysqli_connect_error();
-// }
 
 ?>
