e1ded13184f09eb8d45984794cb7a43cdf6aa10f
B.Naveen Bharathi <144258519+bnaveenbharathi@users.noreply.github.com>
2025-09-05 09:58:55 +0530

    update event dashboard'

--- pages/eventCoordinatorDashboard.php
+++ pages/eventCoordinatorDashboard.php
@@ -257,6 +257,12 @@ if ($data['is_group'] == '0') {
                     <div class="navi-text">VIOLET VIPERS</div>
                 </button>
             </div>
+            <div class="navi-item4">
+                <button class="nav-button" onclick="populateItems(eventData, 'EMERALD EAGLES')" id="emerald">
+                    <div class="navi-icon"><i class="fa-solid fa-staff-snake fa-flip"></i></div>
+                    <div class="navi-text">EMERALD EAGLES</div>
+                </button>
+            </div>
         </div>
         <div class="menu">
 
