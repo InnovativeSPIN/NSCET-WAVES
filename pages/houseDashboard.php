8c1cd4f51c82f9b20ee5485a6ffb3853641dd1da
B.Naveen Bharathi <144258519+bnaveenbharathi@users.noreply.github.com>
2025-09-09 16:06:01 +0530

    update

--- pages/houseDashboard.php
+++ pages/houseDashboard.php
@@ -366,9 +366,7 @@ echo '<img src="' . $path . $img . '.png" class="card-img-top" alt="...">';
                                         </td>
 
                                         <td>
-                                            <ul class="action-list">
-<li><a href=<?php echo './studentRegisteration.php' . "?eventName=" . urlencode($eventName) ?> data-tip="edit"><i class="fa fa-edit"></i></a></li>
-</ul> -->
+                                           
                                         </td>
                                     </tr>
 
